9ffa10f7ac5414079fd4b6aaeb914799a46b94b0
Richard LaFranchi <rlafranchi@icloud.com>
2018-06-05 20:18:39 -0600

    v 1.0

---
+++ README.md
@@ -0,0 +1,133 @@
+# VuexRailsPlugin
+
+### A Vuex plugin that easily maps Rails resources to Vuex modules
+
+The idea of this plugin is to easily incorporate conventional rest endpoints defined when scaffolding Rails resources and map the responses to appropriate state in Vuex. ex. `resources :posts` generates the following endpoints:
+
+ * GET|POST /posts
+ * GET|PUT /posts/:id
+ * DELETE /posts/:id
+
+### Install
+
+```bash
+$ npm i vuex-rails-plugin
+```
+
+### Usage
+
+The plugin can be imported into any Vuex store like so:
+
+```js
+// store.js
+import Vuex from 'vuex'
+import Vue from 'vue
+import VuexRailsPlugin from 'vuex-rails-plugin/src/VuexRailsPlugin'
+
+export default new Vuex.Store({
+  // ...
+  plugins: [
+    VuexRailsPlugin('posts'),
+    VuexRailsPlugin('categories')
+  ]
+})
+```
+
+This example defines Vuex modules for 'posts' and 'categories' namespaces.
+It supports the following state:
+
+ * all - items returned from the index action
+ * current - set when the show action is called
+ * error - set if any errors occur such as validations
+ 
+The state can be mapped to any Vue component using `mapState`
+
+```js
+// some-component.vue
+import { mapState } from 'vuex'
+export default {
+  computed: {
+    ...mapState('posts', {
+      posts: state => state.all,
+      currentPost: state => state.current
+      error: state => state.error
+    })
+  }
+}
+
+```
+
+
+Actions can also be called by using `mapActions`
+
+```js
+// some-component.vue
+import { mapActions } from 'vuex'
+export default {
+  // ...
+  methods: {
+    ...mapActions('posts', {
+      getPosts: 'getAll', // ex. this.getAll()
+      getPost: 'get', // ex. this.getPost(id)
+      createPost: 'create', // ex. this.createPost({title: 'Bad', body: 'Ass'})
+      updatePost: 'update', // ex. this.updatePost({id: 1, title: 'Bad', body: 'Mama Jama'})
+      destroyPost: 'destroy' // ex. this.destroyPost({})
+    })
+  }
+}
+```
+
+Params are supported
+
+```js
+this.getPosts({page: 1, limit: 10})
+```
+
+Use directly in a form
+
+```js
+export default {
+  data() {
+    return {
+      newPost: {
+        title: '',
+        body: ''
+      }
+    }
+  }
+}
+```
+
+```html
+<form @submit.prevent="createPost(newPost)">
+  <label>Title</label>
+  <input type="text" v-model="newPost.title"/>
+  <label>Body</label>
+  <input type="text" v-model="newPost.body">
+  <buton type="submit">Submit</buton>
+</form>
+```
+
+Promises supported
+
+```js
+export default {
+  // ...
+  methods: {
+    promiseMeThisPostWillCreate() {
+      this.createPost(this.newPost)
+        .then(createdPost => {
+          alert('Thanks for keeping your promise!')
+        })
+        .catch(err => {
+          alert('Why did you break my promise?')
+        })
+    }
+  }
+}
+```
+
+### TODO
+
+* Support nested resources ex. `/categories/:category_id/posts`
+* Support other options that may be necessary to support custom getters, filters, etc.
---
+++ src/VuexRailsPlugin.js
@@ -0,0 +1,160 @@
+import axios from 'axios'
+
+const http = axios.create({
+  headers: {
+    'Content-Type': 'application/json'
+  }
+})
+
+http.interceptors.request.use(function (config) {
+  config.headers['X-CSRF-TOKEN'] = document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+  return config
+}, function (error) {
+  return Promise.reject(error.response)
+})
+
+const ALL = 'ALL'
+const GET = 'GET'
+const CREATE = 'CREATE'
+const UPDATE = 'UPDATE'
+const DESTROY = 'DESTROY'
+const ERROR = 'ERROR'
+
+const updateItem = (item, all, remove) => {
+  remove = remove || false
+  const thisItem = all.find(i => i.id === item.id)
+  const thisItemIndex = all.indexOf(thisItem)
+  if (thisItemIndex > -1) {
+    if (remove) {
+      all.splice(thisItemIndex, 1)
+    } else {
+      all.splice(thisItemIndex, 1, item)
+    }
+  }
+}
+
+const resourcePath = (resource, params) => {
+  if (params.id) {
+    return `/${resource}/${params.id}.json`
+  } else {
+    return `/${resource}.json`
+  }
+}
+
+export default function VuexRailsPlugin(resource) {
+  return store => {
+    store.registerModule(resource, {
+      namespaced: true,
+      state: {
+        all: [],
+        current: {},
+        error: null
+      },
+      actions: {
+        getAll({ commit }, params) {
+          params = params || {}
+          return new Promise((resolve, reject) => {
+            http.get(`/${resource}.json`, { params })
+              .then(res => {
+                const items = res.data
+                commit(ALL, { items })
+                resolve(res)
+              })
+              .catch(err => {
+                commit(ERROR, { err })
+                reject(err)
+              })
+          })
+        },
+        get({ commit }, id, params) {
+          params = params || {}
+          return new Promise((resolve, reject) => {
+            http.get(resourcePath(resource, { id: id }), { params })
+              .then(res => {
+                const item = res.data
+                commit(GET, { item })
+                resolve(res)
+              })
+              .catch(err => {
+                commit(ERROR, { err })
+                reject(err)
+              })
+          })
+        },
+        create({ commit }, item) {
+          return new Promise((resolve, reject) => {
+            http.post(`/${resource}.json`, item)
+              .then(res => {
+                const item = res.data
+                commit(CREATE, { item })
+                resolve(res)
+              })
+              .catch(err => {
+                commit(ERROR, { err })
+                reject(err)
+              })
+          })
+        },
+        update({ commit }, item) {
+          return new Promise((resolve, reject) => {
+            http.put(resourcePath(resource, item), item)
+              .then(res => {
+                const item = res.data
+                commit(UPDATE, { item })
+                resolve(res)
+              })
+              .catch(err => {
+                commit(ERROR, { err })
+                reject(err)
+              })
+          })
+        },
+        destroy({ commit }, item) {
+          return new Promise((resolve, reject) => {
+            http.delete(resourcePath(resource, item))
+              .then(res => {
+                commit(DESTROY, { item })
+                resolve(res)
+              })
+              .catch(err => {
+                commit(ERROR, { err })
+                reject(err)
+              })
+          })
+        }
+      },
+      mutations: {
+        [ALL] (state, { items }) {
+          state.error = null
+          state.all = items
+        },
+        [GET] (state, { item }) {
+          state.error = null
+          state.current = item
+          updateItem(state.current, state.all)
+        },
+        [CREATE] (state, { item }) {
+          state.error = null
+          if (state.all.find(i => i.id === item.id) !== undefined) {
+            updateItem(item, state.all)
+          } else {
+            state.all.push(item)
+          }
+        },
+        [UPDATE] (state, { item }) {
+          state.error = null
+          state.current = null
+          updateItem(item, state.all)
+        },
+        [DESTROY] (state, { item }) {
+          state.error = null
+          state.current = null
+          updateItem(item, state.all, true)
+        },
+        [ERROR] (state, { err }) {
+          state.error = err.response && err.response.data ? err.response.data : err
+        }
+      }
+    })
+  }
+}
